9bb3cfb96f08eac46e735f0888e4d772e810a096
Sam Morningstar <sammorningstar5064@gmail.com>
2022-04-10 16:23:33 +0400

    JS Resetup

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app1.listen(port);
 
 MongoClient.connect(
-  "mongodb+srv://ASDF:ASDF@cluster0.2a6e0.mongodb.net/CW?retryWrites=true&w=majority",
+  "mongodb+srv://ASDF:ASDF@cluster0.2a6e0.mongodb.net/CW",
   (err, client) => {
     db = client.db("CW");
   }
