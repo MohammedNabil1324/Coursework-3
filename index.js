a4c43d5eaf82e7ed4493d3affa055c4699043bb0
Sam Morningstar <sammorningstar5064@gmail.com>
2022-04-11 02:04:33 +0400

    Changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-var MongoClient = require("mongodb").MongoClient;
-var http = require("http");
+ const express = require("express");
+ const path = require("path");
+ const fs = require("fs");
+ const MongoClient = require("mongodb").MongoClient;
+ const http = require("http");
 
 var app1 = express();
 let db;
