6a91cd014c2d2e0c29622393fd0907175ef6da6c
Sam Morningstar <sammorningstar5064@gmail.com>
2022-04-11 00:07:05 +0400

    CORS Setup

--- JS/Frontend.js
+++ JS/Frontend.js
@@ -4,9 +4,9 @@ let app = new Vue({
       lessons:{},
     },
     created: function () {
-        fetch("https://coursewrk-2.herokuapp.com/collection/Lessons").then(
+        fetch("https://coursew3.herokuapp.com/collection/Lessons").then(
           function (response) {
-              response.setHeader('Access-Control-Allow-Origin','*');
+              response.setHeader('Access-Control-Allow-Origin',req.headers.origin);
               response.json.then(function (json) {
               app.lessons = json;
             });
