8b623ab28c9a474b3ad28d32d0728a470d2bc34e
Sam Morningstar <sammorningstar5064@gmail.com>
2022-04-11 11:56:03 +0400

    Fetch

--- JS/Frontend.js
+++ JS/Frontend.js
@@ -1,15 +1,6 @@
 let app = new Vue({
   data: {
     sitename: "Lessons",
-    lessons:[],
+    lessons:{}
   },
-  created: function () {
-    fetch('https://coursew3.herokuapp.com/collection/Lessons').then(
-        function (response, req) {
-           response.json().then(function (json) {
-               app.lessons = json;
-               console.log(app.lessons);
-      });
-    })
-  }
 })
