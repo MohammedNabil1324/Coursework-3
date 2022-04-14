1473042d041be44faf7a65ce9feed408d8da6a6b
Sam Morningstar <sammorningstar5064@gmail.com>
2022-04-14 15:44:19 +0400

    Service

--- JS/service-worker.js
+++ JS/service-worker.js
@@ -1,4 +1,24 @@
-var cacheName = 'classes';
+var cacheName = "classes";
+var cacheFiles = [
+  "index.html",
+  "index.js",
+  "index.webmanifest",
+  "Assets/Bio.jpg",
+  "Assets/CompSc.jpg",
+  "Assets/English.jpg",
+  "Assets/Math.jpg",
+  "Assets/mortarboard.png",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
 
 self.addEventListener("fetch", function (e) {
   e.respondWith(
