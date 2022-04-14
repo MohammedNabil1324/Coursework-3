2ce082f3a511ff1a1a0aa170183a2e71a77b95af
Sam Morningstar <sammorningstar5064@gmail.com>
2022-04-14 13:11:42 +0400

    Cache

--- JS/service-worker.js
+++ JS/service-worker.js
@@ -1,7 +1,18 @@
+var cacheName = 'classes';
+
 self.addEventListener("fetch", function (e) {
   e.respondWith(
     caches.match(e.request).then(function (r) {
-        return (
+      console.log("[Service Worker] Fetching resource: " + e.request.url);
+      return r;
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return (
         r ||
         fetch(e.request).then(function (response) {
           return caches.open(cacheName).then(function (cache) {
