bc9fc5dc469d0e5c4e272b97d8967f865e4f5116
SamMorningstar <sammorningstar5064@gmail.com>
2022-04-18 14:19:16 +0400

    Test

---
+++ src/Components/Lessons.vue
@@ -0,0 +1,33 @@
+<template>
+  <main v-for="lesson in lesson" :key="lesson.id">
+    <h2>{{ lesson.subject }}</h2>
+    <span id="icon"><i :class="lesson.icon"></i></span>
+    <p>Location:{{ lesson.location }}</p>
+    <p>Price:£{{ lesson.price }}</p>
+    <p>Space:{{ lesson.space }}</p>
+    <img id="img" v-bind:src="lesson.image" /><br />
+    <button
+      :id="lesson.id - 1"
+      @click="addLesson(lesson)"
+      :disabled="lesson.space == 0"
+    >
+      Add to Cart
+    </button>
+    <p :disabled="lesson.space != 0">Out of Stock</p>
+    <br />
+  </main>
+</template>
+<script>
+export default {
+  name: "Lessons",
+  props: ['lesson'],
+  data() {
+    return {};
+  },
+  methods: {
+    add(lesson) {
+      this.$emit("addlesson", lesson);
+    },
+  },
+};
+</script>
---
+++ src/service-worker.js
@@ -0,0 +1,46 @@
+var cacheName = "classes";
+var cacheFiles = [
+  "index.html",
+  "index.js",
+  "index.webmanifest",
+  "Assets/Bio.jpg",
+  "Assets/CompSc.jpg",
+  "Assets/English.jpg",
+  "Assets/Math.jpeg",
+  "Assets/mortarboard.png",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      console.log("[Service Worker] Fetching resource: " + e.request.url);
+      return r;
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
